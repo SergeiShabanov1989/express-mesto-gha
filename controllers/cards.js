1279ba7ec9b1666fb32c8a559419ddf77001e828
Sergei Shabanov <shabanov1989@gmail.com>
2022-06-22 23:41:32 +0300

    feat: добавил обработку ошибок для карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,18 +10,31 @@ module.exports.createCard = async (req, res) => {
     const { name, link } = req.body;
 
     const newCard = await Card.create({ name, link, owner: req.user._id })
-    res.send(newCard)
+    res.status(200).send(newCard)
   } catch (err) {
-    res.status(400).send({message: "Переданы некорректные данные при создании карточки"})
+    if (err.name === 'ValidationError') {
+      res.status(400).send({message: "Переданы некорректные данные при создании карточки"})
+      return
+    }
+    res.status(500).send({message: "Ошибка по умолчанию"})
   }
 };
 
 module.exports.deleteCard = async (req, res) => {
   try {
     const card = await Card.findByIdAndRemove(req.params.cardId)
-    res.send(card)
+      .orFail(() => new Error('Not Found'))
+    res.status(200).send(card)
   } catch (err) {
-    res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+    if (err.message === 'Not Found') {
+      res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+      return
+    }
+    if (err.name === 'CastError') {
+      res.status(400).send({message: "Некорректно передан id"})
+      return
+    }
+    res.status(500).send({message: "Ошибка по умолчанию"})
   }
 };
 
@@ -32,9 +45,18 @@ module.exports.likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true }
     )
-    res.send(card)
+      .orFail(() => new Error('Not Found'))
+    res.status(200).send(card)
   } catch (err) {
-    res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+    if (err.message === 'Not Found') {
+      res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+      return
+    }
+    if (err.name === 'CastError') {
+      res.status(400).send({message: "Некорректно передан id"})
+      return
+    }
+    res.status(500).send({message: "Ошибка по умолчанию"})
   }
 };
 
@@ -45,8 +67,17 @@ module.exports.dislikeCard = async (req, res) => {
       { $pull: { likes: req.user._id } },
       { new: true }
     )
+      .orFail(() => new Error('Not Found'))
     res.send(card)
   } catch (err) {
-    res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+    if (err.message === 'Not Found') {
+      res.status(404).send({message: "Запрашиваемая карточка не найдена"})
+      return
+    }
+    if (err.name === 'CastError') {
+      res.status(400).send({message: "Некорректно передан id"})
+      return
+    }
+    res.status(500).send({message: "Ошибка по умолчанию"})
   }
 };
