7741f61f5326da10ab06d1a056a0b7363b0f003d
Sergei Shabanov <shabanov1989@gmail.com>
2022-07-04 16:27:52 +0300

    fix: исправил ошибки тестов

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post('/signup', celebrate({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
     avatar: Joi.string().required().regex(/(https?:\/\/|ftps?:\/\/|www\.)((?![.,?!;:()]*(\s|$))[^\s]){2,}/),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), createUser);
