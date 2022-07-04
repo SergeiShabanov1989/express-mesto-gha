602a85f47167c018570e233830f21a0f44343f8a
Sergei Shabanov <shabanov1989@gmail.com>
2022-07-04 16:41:00 +0300

    fix: исправил ошибки тестов

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().regex(/(https?:\/\/|ftps?:\/\/|www\.)((?![.,?!;:()]*(\s|$))[^\s]){2,}/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/(https?:\/\/|ftps?:\/\/|www\.)((?![.,?!;:()]*(\s|$))[^\s]){2,}/),
+    email: Joi.string().required().email().min(1),
+    password: Joi.string().required().min(1),
   }),
 }), createUser);
 
